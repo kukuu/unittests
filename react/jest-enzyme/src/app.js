4d141ee55110e74c8fcc11a2b1531204d79e322e
Alexander Adu-Sarkodie <aadusarkodie1@gmail.com>
2019-05-20 13:48:38 +0100

    Unit tests with enzyme, jest.

---
+++ react/jest-enzyme/src/app.js
@@ -0,0 +1,17 @@
+
+import React, { Component } from 'react';
+
+
+Class App extends Component {
+	
+	render(){
+		
+		return (
+			<div>
+				<h2> Testing Components</h2>
+			</div>
+		)
+	}
+}
+
+export default App;
---
+++ react/jest-enzyme/src/app.test.js
@@ -0,0 +1,10 @@
+import eact from 'react';
+import App from './app';
+import { shallow } from 'enzyme';
+
+
+describe("<App />", () => {
+	it("Renders 1 <App /> component", () => {
+		
+	})
+})
