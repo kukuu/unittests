7dcff81625c04b8149dcca830070b7198075f982
Alexander Adu-Sarkodie <aadusarkodie1@gmail.com>
2019-07-16 17:30:16 +0100

    Added more context in commented areas.

--- mocha-chai/test/rockPaperScissors.js
+++ mocha-chai/test/rockPaperScissors.js
@@ -1,20 +1,20 @@
 
 module.exports = function(user, comp){
-	//Note: a can be rock,paper scissors
-	//b can also be rock, paper or scissors
+	//Note: user can be rock,paper scissors
+	//comp can also be rock, paper or scissors
 
 	//1. both players play the same thing
 	if(user === user){
 		return "draw";
 	}
 
-	//2. Condition where "a" wins
+	//2. Condition where "user" wins
 	if((user == 'rock' && comp === 'scissors') || 
 		(user === 'paper' && comp == 'rock') || 
 		(user === 'scissors' && comp === 'paper') {
 			return 'user';
 	}
 
-	//2. Condition where "b" wins
+	//2. Condition where computer, "comp" wins
 	return 'comp';
 };
